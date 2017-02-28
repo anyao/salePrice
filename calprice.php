e9eb52368eeb4fcf3aab1cb8d1e82ab86f145d19
An Yao <imanyao@icloud.com>
2017-02-28 16:15:20 +0800

    bug改完钦此

--- calprice.php
+++ calprice.php
@@ -80,19 +80,21 @@ class calPrice{
         // 宽度的pgrade判断
         if (in_array($pgrade, array_keys($widthArr))) 
             // 如果存在其型号的宽度记录
-            $arrWidth = $widthArr[$pgrade][$i];
-        else if (strpos($pgrade,"出口") >= 0) 
+            $arrWidth = $widthArr[$pgrade];
+        else if (strpos($pgrade,"出口") !== false) 
             $arrWidth = $widthArr['出口'];
-        else if (strpos($pgrade,"船板") >= 0) 
+        else if (strpos($pgrade,"船板") !== false) 
             $arrWidth = $widthArr['船板'];
-        else
+        else{
             $arrWidth = $widthArr['内贸'];
+            if(!empty($zlevel))
+                $ifZW = true;
+        }
 
-        // $ifZ = empty($zlevel); Z向性能宽度不加价？？？忘了。
 
-        if ($ifZ) {
+        if (isset($ifZW)) {
             $price['width'] = 0;
-            $msg .= "宽度 $width 加价：".$price['width']." ";
+            $msg .= "宽度 $width 加价：z向钢免收定尺费 ";
         }elseif ($width <= 1800 && $width >= 3210)
         // 宽度不符合要求
             return array('errmsg' => '宽度不符合要求');
@@ -109,43 +111,56 @@ class calPrice{
 
 
         // 长度的pgrade判断
-        if (strpos($pgrade,"出口") >= 0) 
+        if (in_array($pgrade, array_keys($lengthArr))) 
+            // 如果存在其型号的宽度记录
+            $arrLength = $lengthArr[$pgrade];
+        else if (strpos($pgrade,"出口") !== false) {
             $arrLength = $lengthArr['出口'];
-        else if (strpos($pgrade,"船板") >= 0) 
+        }
+        else if (strpos($pgrade,"船板") !== false) 
             $arrLength = $lengthArr['船板'];
-        else
+        else{
             $arrLength = $lengthArr['内贸'];
+            if(!empty($zlevel))
+                $ifZ = true;
+        }
 
-        for ($i=0; $i < count($arrLength); $i++) 
-            $rLength = $arrLength[$i];
-            // 内贸---长度加价分2种情况：1、指定长度（值） 2、指定长度范围（a、“8米以上”不加价 b、非常规指定长度范围）
-            if ($typeLength == "指定长度") 
-                // 指定长度值
-                if ($rLength['minlength'] <= $length && $rLength['maxlength'] >= $length ) {
-                    $price['length'] = $rLength['price'];
-                    $msg .= "长度 $length 加价：".$price['length']." ";
-                    break;
-                }else
-                    // 指定长度值不符合范围
-                    if ($i == count($arrLength)-1) 
-                        return array('errmsg' => '指定长度值不符合范围');
-            elseif($length >= 8000){
-                // $typeLength == "指定长度范围" 其范围大于8米
-                $price['length'] = 0;
-                $msg .= "长度 $length 加价：".$price['length']." ";
-                
-            }else
-                if ($rLength['minlength'] <= $length && $rLength['maxlength'] >= $length) {
-                    $price['length'] = $rLength['extraprice'];
-                    $msg .= "长度 $length 加价：".$price['length']." ";
-                    break;
+
+        if (isset($ifZ)) {
+           $price['length'] = 0;
+            $msg .= "宽度 $width 加价：z向钢免收定尺费 "; 
+        }else
+            for ($i=0; $i < count($arrLength); $i++) {
+                $rLength = $arrLength[$i];
+                // 内贸---长度加价分2种情况：1、指定长度（值） 2、指定长度范围（a、“8米以上”不加价 b、非常规指定长度范围）
+                if ($typeLength == "指定长度"){
+                    // 指定长度值
+                    if ($rLength['minlength'] <= $length && $rLength['maxlength'] >= $length ) {
+                        $price['length'] = $rLength['price'];
+                        $msg .= "长度 $length 加价：".$price['length']." ";
+                        break;
+                    }else
+                        // 指定长度值不符合范围
+                        if ($i == count($arrLength)-1) 
+                            return array('errmsg' => '指定长度值不符合范围');
                 }
-            
+                elseif($length >= 8000){
+                    // $typeLength == "指定长度范围" 其范围大于8米
+                    $price['length'] = 0;
+                    $msg .= "长度 $length 加价：".$price['length']." ";
+                }else
+                    if ($rLength['minlength'] <= $length && $rLength['maxlength'] >= $length) {
+                        $price['length'] = $rLength['extraprice'];
+                        $msg .= "长度 $length 加价：".$price['length']." ";
+                        break;
+                    }
+            } 
 
         // z向性能性能加价
         if (empty($zlevel)){
             // z向性能无要求
             $price['zlevel'] = 0;
+            $msg .= "Z向性能 $zlevel 加价：".$price['zlevel']." ";
         }else
             for ($i=0; $i < count($zFunctionArr); $i++) {
                 $rZ = $zFunctionArr[$i];
@@ -174,7 +189,11 @@ class calPrice{
                     // 探伤性能错误
                     if ($i == count($probeArr)-1) 
                         return array('errmsg' => '探伤性能错误');    
-            }    
+            } 
+            if ($zlevel) {
+                   $price['tslevel'] -= 100;
+                   $msg .= "探伤{$tslevel}级 加价：".$price['tslevel']." ";
+            }   
         }
         // print_r($price);
         // die;
